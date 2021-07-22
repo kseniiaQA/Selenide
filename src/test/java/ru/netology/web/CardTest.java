7e508e093ab999f5808e8013d344834fa23e4169
Your Name <you@example.com>
2021-07-22 15:05:30 +0300

    tests work

---
+++ src/test/java/ru/netology/web/CardTest.java
@@ -0,0 +1,41 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardTest {
+    //    java -jar artifacts\app-order.jar
+
+//    @BeforeAll
+//    void setUp(){
+//        $(chrome --headless --remote-debugging-port=9222 chromium.org);
+//    }
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Елена");
+        form.$("[data-test-id=phone] input").setValue("+79340000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldPass() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Алла-Мария");
+        form.$("[data-test-id=phone] input").setValue("+79868758413");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
+
