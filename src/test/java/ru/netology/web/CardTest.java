e99340ff09f2dedd2f1cfaf1f4fe9150efdb9e3e
Your Name <you@example.com>
2021-07-27 12:59:36 +0300

    commit

--- src/test/java/ru/netology/web/CardTest.java
+++ src/test/java/ru/netology/web/CardTest.java
@@ -17,35 +17,57 @@ public class CardTest {
 
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Елена");
         form.$("[data-test-id=phone] input").setValue("+79540000000");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldPass() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Алла-Мария");
         form.$("[data-test-id=phone] input").setValue("+79868758413");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
     void shouldNotPass() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Алла1");
         form.$("[data-test-id=phone] input").setValue("+79868758413");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $("[class=input__sub]").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $$(".input__sub").get(0).shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
+    @Test
+    void shouldNotPass1() {
+
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Алла");
+        form.$("[data-test-id=phone] input").setValue("+7986875841");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $$(".input__sub").get(1).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotPass2() {
+
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Алла");
+        form.$("[data-test-id=phone] input").setValue("+79868758413");
+
+        form.$("[role=button]").click();
+        form.$(".input_invalid").exists();
+    }
 }
+
